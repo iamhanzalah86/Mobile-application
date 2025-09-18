850ee0ca063038111c9ad803e5135aacf03f9487
Hanzalah Jamal <hanzalahjamal86@gmail.com>
2025-09-18 11:46:09 +0500

    changes

--- task3/bin/task3.dart
+++ task3/bin/task3.dart
@@ -6,8 +6,8 @@ void main() {
 
   for (int i = 1; i <= n; i++) {
     for (int j = 1; j <= i; j++) {
-      print("$j ");
+      stdout.write("$j ");
     }
-    print(" "); // move to next line
+    print(""); // move to next line
   }
 }
